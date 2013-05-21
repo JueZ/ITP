f7594981141efd16dcf033617cc40df03c1c41b7
Martin <mkoschi@me.com>
2013-05-21 23:51:11 +0200

    Initial Import

---
+++ Akanonda/resetForm.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace Akanonda
+{
+    partial class resetForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SurvivalTimeBox = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            this.richTextBox1.BackColor = System.Drawing.SystemColors.Control;
+            this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox1.Cursor = System.Windows.Forms.Cursors.No;
+            this.richTextBox1.Enabled = false;
+            this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox1.ImeMode = System.Windows.Forms.ImeMode.Off;
+            this.richTextBox1.Location = new System.Drawing.Point(87, 64);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.None;
+            this.richTextBox1.Size = new System.Drawing.Size(96, 21);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "RESTART?";
+            // 
+            // button1
+            // 
+            this.button1.DialogResult = System.Windows.Forms.DialogResult.Yes;
+            this.button1.Location = new System.Drawing.Point(31, 111);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "YES";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.DialogResult = System.Windows.Forms.DialogResult.No;
+            this.button2.Location = new System.Drawing.Point(137, 111);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "NO";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // SurvivalTimeBox
+            // 
+            this.SurvivalTimeBox.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            this.SurvivalTimeBox.BackColor = System.Drawing.SystemColors.Control;
+            this.SurvivalTimeBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.SurvivalTimeBox.Cursor = System.Windows.Forms.Cursors.No;
+            this.SurvivalTimeBox.Enabled = false;
+            this.SurvivalTimeBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SurvivalTimeBox.ImeMode = System.Windows.Forms.ImeMode.Off;
+            this.SurvivalTimeBox.Location = new System.Drawing.Point(47, 22);
+            this.SurvivalTimeBox.Name = "SurvivalTimeBox";
+            this.SurvivalTimeBox.ReadOnly = true;
+            this.SurvivalTimeBox.Size = new System.Drawing.Size(169, 44);
+            this.SurvivalTimeBox.TabIndex = 3;
+            this.SurvivalTimeBox.Text = "";
+            // 
+            // resetForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(242, 146);
+            this.Controls.Add(this.SurvivalTimeBox);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "resetForm";
+            this.Text = "Game Over";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RichTextBox SurvivalTimeBox;
+    }
+}
---
+++ Akanonda/resetForm.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Akanonda
+{
+    public partial class resetForm : Form
+    {
+        public resetForm(int seconds, int minutes)
+        {
+            InitializeComponent();
+            if (minutes == 0)
+            {
+                SurvivalTimeBox.Text = seconds > 1 || seconds == 0 ? "          GAME OVER!\nYou survived " + seconds + " seconds!" : "         GAME OVER!\nYou survived " + seconds + " second!";
+            }
+            else
+            {
+                SurvivalTimeBox.Text = minutes > 1 ? "      GAME OVER!\nYou survived " + seconds + " seconds and " + minutes + " minutes!" : "GAME OVER!\nYou survived " + seconds + " seconds and " + minutes + " minute!";
+            }
+        }
+
+
+    }
+}
