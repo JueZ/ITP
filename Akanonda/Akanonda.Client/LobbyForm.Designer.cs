3c6d9bbd54951cfc8337ae5bac36b6d696c653ec
Matthias <mdick@gmx.at>
2013-06-21 12:12:06 +0200

    Lobby Form with chat

---
+++ Akanonda/Akanonda.Client/LobbyForm.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace Akanonda
+{
+    partial class LobbyForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.ChatBox = new System.Windows.Forms.TextBox();
+            this.MessageBox = new System.Windows.Forms.TextBox();
+            this.SendButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.45F, System.Drawing.FontStyle.Bold);
+            this.button1.Location = new System.Drawing.Point(635, 425);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 40);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start Playing";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // ChatBox
+            // 
+            this.ChatBox.Location = new System.Drawing.Point(12, 82);
+            this.ChatBox.Multiline = true;
+            this.ChatBox.Name = "ChatBox";
+            this.ChatBox.Size = new System.Drawing.Size(243, 213);
+            this.ChatBox.TabIndex = 1;
+            // 
+            // MessageBox
+            // 
+            this.MessageBox.Location = new System.Drawing.Point(12, 301);
+            this.MessageBox.Name = "MessageBox";
+            this.MessageBox.Size = new System.Drawing.Size(162, 20);
+            this.MessageBox.TabIndex = 2;
+            // 
+            // SendButton
+            // 
+            this.SendButton.Location = new System.Drawing.Point(180, 301);
+            this.SendButton.Name = "SendButton";
+            this.SendButton.Size = new System.Drawing.Size(75, 20);
+            this.SendButton.TabIndex = 3;
+            this.SendButton.Text = "Send";
+            this.SendButton.UseVisualStyleBackColor = true;
+            this.SendButton.Click += new System.EventHandler(this.SendButton_Click);
+            // 
+            // LobbyForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(759, 477);
+            this.Controls.Add(this.SendButton);
+            this.Controls.Add(this.MessageBox);
+            this.Controls.Add(this.ChatBox);
+            this.Controls.Add(this.button1);
+            this.Name = "LobbyForm";
+            this.Text = "Akanonda Lobby";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox ChatBox;
+        private System.Windows.Forms.TextBox MessageBox;
+        private System.Windows.Forms.Button SendButton;
+    }
+}
