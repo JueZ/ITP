9c047b2445d8164b5e165f0cd8661e4489a35dbd
Matthias <mdick@gmx.at>
2013-06-21 13:41:50 +0200

    LobbyForm with chatclient working

--- Akanonda/Akanonda.Client/LobbyForm.cs
+++ Akanonda/Akanonda.Client/LobbyForm.cs
@@ -6,51 +6,140 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
-using System.Net.Sockets ;
+using System.Net.Sockets;
 using System.Threading;
+
+using Lidgren.Network;
 
 namespace Akanonda
 {
     public partial class LobbyForm : Form
     {
+
+        private static NetClient s_client;
+        private static LobbyForm s_form;
+
         public LobbyForm()
         {
             InitializeComponent();
+
+            startChat();
+            MessageBox.KeyDown += new KeyEventHandler(MessageBox_KeyDown);
+
+
         }
 
-        System.Net.Sockets.TcpClient clientSocket = new System.Net.Sockets.TcpClient();
-        NetworkStream serverStream = default(NetworkStream);
-        string readData = null;
-
-        private void SendButton_Click(object sender, EventArgs e)
+        [STAThread]
+        static void startChat()
         {
-            byte[] outStream = System.Text.Encoding.ASCII.GetBytes(MessageBox.Text + "$");
-            serverStream.Write(outStream, 0, outStream.Length);
-            serverStream.Flush();
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            s_form = new LobbyForm();
+
+            NetPeerConfiguration config = new NetPeerConfiguration("chat");
+            config.AutoFlushSendQueue = false;
+            s_client = new NetClient(config);
+
+            s_client.RegisterReceivedCallback(new SendOrPostCallback(GotMessage));
+
+            int port;
+            Int32.TryParse("14242", out port);
+            Connect("localhost", port);
+
+
+            Application.Run(s_form);
+
+            //s_client.Shutdown("Bye");
         }
 
-        private void getMessage()
+        public static void Connect(string host, int port)
         {
-            while (true)
+            s_client.Start();
+            NetOutgoingMessage hail = s_client.CreateMessage("This is the hail message");
+            s_client.Connect(host, port, hail);
+        }
+
+        void MessageBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Return || e.KeyCode == Keys.Enter)
             {
-                serverStream = clientSocket.GetStream();
-                int buffSize = 0;
-                byte[] inStream = new byte[10025];
-                buffSize = clientSocket.ReceiveBufferSize;
-                serverStream.Read(inStream, 0, buffSize);
-                string returndata = System.Text.Encoding.ASCII.GetString(inStream);
-                readData = "" + returndata;
-                msg();
+                // return is equivalent to clicking "send"
+                SendButton_Click(sender, e);
             }
         }
 
-        private void msg()
+        private void SendButton_Click(object sender, EventArgs e)
         {
-            if (this.InvokeRequired)
-                this.Invoke(new MethodInvoker(msg));
-            else
-                ChatBox.Text = ChatBox.Text + Environment.NewLine + " >> " + readData;
-        }         
+            // Send
+            if (!string.IsNullOrEmpty(MessageBox.Text))
+                Send(MessageBox.Text);
+            MessageBox.Text = "";
+
+        }
+
+        public static void Send(string text)
+        {
+            NetOutgoingMessage om = s_client.CreateMessage(text);
+            s_client.SendMessage(om, NetDeliveryMethod.ReliableOrdered);
+            Output("Sending '" + text + "'");
+            s_client.FlushSendQueue();
+        }
+
+        private static void Output(string text)
+        {
+            s_form.ChatBox.Text += "\r\n" + text.ToString();
+        }
+
+        public static void GotMessage(object peer)
+        {
+            NetIncomingMessage im;
+            while ((im = s_client.ReadMessage()) != null)
+            {
+                // handle incoming message
+                switch (im.MessageType)
+                {
+                    case NetIncomingMessageType.DebugMessage:
+                    case NetIncomingMessageType.ErrorMessage:
+                    case NetIncomingMessageType.WarningMessage:
+                    case NetIncomingMessageType.VerboseDebugMessage:
+                        string text = im.ReadString();
+                        Output(text);
+                        break;
+                    case NetIncomingMessageType.StatusChanged:
+                        NetConnectionStatus status = (NetConnectionStatus)im.ReadByte();
+
+                        if (status == NetConnectionStatus.Connected)
+                            s_form.EnableInput();
+                        else
+                            s_form.DisableInput();
+
+
+                        string reason = im.ReadString();
+                        Output(status.ToString() + ": " + reason);
+
+                        break;
+                    case NetIncomingMessageType.Data:
+                        string chat = im.ReadString();
+                        Output(chat);
+                        break;
+                    default:
+                        Output("Unhandled type: " + im.MessageType + " " + im.LengthBytes + " bytes");
+                        break;
+                }
+            }
+        }
+
+        public void EnableInput()
+        {
+            MessageBox.Enabled = true;
+            SendButton.Enabled = true;
+        }
+
+        public void DisableInput()
+        {
+            MessageBox.Enabled = false;
+            SendButton.Enabled = false;
+        }
 
 
     }
