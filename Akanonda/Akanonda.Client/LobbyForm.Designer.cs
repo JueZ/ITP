d700e7abd4506eac024f1f6b3406bdcd1919869d
Matthias <mdick@gmx.at>
2013-06-21 16:05:46 +0200

    Lobby ListViews

--- Akanonda/Akanonda.Client/LobbyForm.Designer.cs
+++ Akanonda/Akanonda.Client/LobbyForm.Designer.cs
@@ -32,12 +32,17 @@
             this.MessageBox = new System.Windows.Forms.TextBox();
             this.SendButton = new System.Windows.Forms.Button();
             this.ChatBox = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.PlayersInLobby = new System.Windows.Forms.ListBox();
+            this.PlayersInGame = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.45F, System.Drawing.FontStyle.Bold);
-            this.button1.Location = new System.Drawing.Point(635, 425);
+            this.button1.Location = new System.Drawing.Point(668, 442);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(112, 40);
             this.button1.TabIndex = 4;
@@ -46,14 +51,14 @@
             // 
             // MessageBox
             // 
-            this.MessageBox.Location = new System.Drawing.Point(12, 301);
+            this.MessageBox.Location = new System.Drawing.Point(11, 416);
             this.MessageBox.Name = "MessageBox";
             this.MessageBox.Size = new System.Drawing.Size(162, 20);
             this.MessageBox.TabIndex = 1;
             // 
             // SendButton
             // 
-            this.SendButton.Location = new System.Drawing.Point(180, 301);
+            this.SendButton.Location = new System.Drawing.Point(179, 416);
             this.SendButton.Name = "SendButton";
             this.SendButton.Size = new System.Drawing.Size(75, 20);
             this.SendButton.TabIndex = 3;
@@ -63,18 +68,66 @@
             // 
             // ChatBox
             // 
-            this.ChatBox.Location = new System.Drawing.Point(12, 24);
+            this.ChatBox.Location = new System.Drawing.Point(11, 133);
             this.ChatBox.Name = "ChatBox";
             this.ChatBox.ReadOnly = true;
-            this.ChatBox.Size = new System.Drawing.Size(243, 271);
+            this.ChatBox.Size = new System.Drawing.Size(243, 277);
             this.ChatBox.TabIndex = 0;
             this.ChatBox.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(8, 117);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Chat";
+            // 
+            // PlayersInLobby
+            // 
+            this.PlayersInLobby.FormattingEnabled = true;
+            this.PlayersInLobby.Location = new System.Drawing.Point(260, 133);
+            this.PlayersInLobby.Name = "PlayersInLobby";
+            this.PlayersInLobby.Size = new System.Drawing.Size(257, 303);
+            this.PlayersInLobby.TabIndex = 6;
+            // 
+            // PlayersInGame
+            // 
+            this.PlayersInGame.FormattingEnabled = true;
+            this.PlayersInGame.Location = new System.Drawing.Point(523, 133);
+            this.PlayersInGame.Name = "PlayersInGame";
+            this.PlayersInGame.Size = new System.Drawing.Size(257, 303);
+            this.PlayersInGame.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(257, 117);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(87, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Players in Lobby:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(520, 117);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(86, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Players in Game:";
             // 
             // LobbyForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(759, 477);
+            this.ClientSize = new System.Drawing.Size(792, 491);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.PlayersInGame);
+            this.Controls.Add(this.PlayersInLobby);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.ChatBox);
             this.Controls.Add(this.SendButton);
             this.Controls.Add(this.MessageBox);
@@ -92,5 +145,10 @@
         private System.Windows.Forms.TextBox MessageBox;
         private System.Windows.Forms.Button SendButton;
         private System.Windows.Forms.RichTextBox ChatBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox PlayersInLobby;
+        private System.Windows.Forms.ListBox PlayersInGame;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
     }
 }
